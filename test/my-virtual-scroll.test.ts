a78967f2d244d7aedf649304eab445e27b8223d4
KIM-MINSOO <minsoo.development@gmail.com>
2022-12-23 15:55:40 +0900

    test init

--- test/my-virtual-scroll.test.ts
+++ test/my-virtual-scroll.test.ts
@@ -1,11 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-
-import {describe, expect, test} from '@jest/globals';
+import {jest, describe, expect, test, beforeEach} from '@jest/globals';
 import MyVirtualScroll from '../src/my-virtual-scroll';
+import { createRowRect, getBeforeBenchSize, getFirstRow } from '../src/utils/rows';
+import { getWrapperStyle } from '../src/utils/styles';
+import type { ScrollRefCoordinates } from '../src/utils/scroll';
+
 
 describe('my virtual scroll', () => {
+  beforeEach(() => {
+    window.scrollTo = jest.fn();
+  });
+
   test('empty load', () => {
     const body = document.body;
     const container = document.createElement('div');
@@ -19,69 +26,84 @@ describe('my virtual scroll', () => {
     expect(myVirtualScroll.getRenderRows().length).toBe(0);
     expect(myVirtualScroll.getRenderFirstRow()).toBe(0);
     expect(myVirtualScroll.getRenderLastRow()).toBe(0);
-
   });
 
-  // test('dynamic load', () => {
-  //   const body = document.body;
-  //   const container = document.createElement('div');
-  //   const wrapper = document.createElement('ul');
-  //   const item = document.createElement('li');
-  //   const rows: Record<string, any>[] = [];
+  // #region utils > rows
+  test('[utils.rows] vertical first row with empty size test', () => {
+    const scroll = 470;
+    const rowSize = 50;
+    const wrapperStartEmptySize = 100;
+    const scrollRefCoordinates: ScrollRefCoordinates = ['top', 'bottom'];
+    const rects = createRowRect(1000, ['top', 'bottom'], wrapperStartEmptySize, rowSize);
+    const firstRow = getFirstRow(rects, scrollRefCoordinates, scroll); // 9
 
-  //   body.appendChild(container);
-  //   container.appendChild(wrapper);
+    expect(firstRow).toBe(7); // 7: 350 ~ 400
+  });
+  
+  test('[utils.rows] vertical bench hide size test', () => {
+    const scroll = 470;
+    const rowSize = 50;
+    const wrapperStartEmptySize = 0;
+    const scrollRefCoordinates: ScrollRefCoordinates = ['top', 'bottom'];
+    const rects = createRowRect(1000, ['top', 'bottom'], wrapperStartEmptySize, rowSize);
+    const firstRow = getFirstRow(rects, scrollRefCoordinates, scroll);
+    const hideSizeForBench0 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 0);
+    const hideSizeForBench1 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 1);
+    const hideSizeForBench2 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 2);
+    const hideSizeForBench3 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 3);
 
-  //   for (let i = 0; i < 1000; i++) {
-  //     const newItem = item.cloneNode();
+    expect(firstRow).toBe(9); // 9: 450 ~ 500
+    expect(hideSizeForBench0).toBe(20); // 20
+    expect(hideSizeForBench1).toBe(70); // 50 + 20
+    expect(hideSizeForBench2).toBe(120); // 100 + 20
+    expect(hideSizeForBench3).toBe(170); // 150 + 20
+  });
 
-  //     newItem.textContent = `${i + 1} 번째 항목입니다.`;
+  test('[utils.rows] horizontal first with empty size', () => {
+    const scroll = 527;
+    const rowSize = 50;
+    const wrapperStartEmptySize = 50;
+    const scrollRefCoordinates: ScrollRefCoordinates = ['left', 'right'];
+    const rects = createRowRect(1000, ['left', 'right'], wrapperStartEmptySize, rowSize);
+    const firstRow = getFirstRow(rects, scrollRefCoordinates, scroll);
 
-  //     wrapper.appendChild(newItem);
-  //     rows.push({
-  //       row: i,
-  //       name: `${i + 1} 번째 항목입니다.`
-  //     });
-  //   }
+    expect(firstRow).toBe(9); // 9: 450 ~ 500
+  });
 
-  //   const myVirtualScroll = new MyVirtualScroll(container, wrapper, {
-  //     rows
-  //   });
-  //   const renderRows = myVirtualScroll.getRenderRows();
+  test('[utils.rows] horizontal bench hide size test', () => {
+    const scroll = 527;
+    const rowSize = 50;
+    const wrapperStartEmptySize = 0;
+    const scrollRefCoordinates: ScrollRefCoordinates = ['left', 'right'];
+    const rects = createRowRect(1000, ['left', 'right'], wrapperStartEmptySize, rowSize);
+    const firstRow = getFirstRow(rects, scrollRefCoordinates, scroll);
+    const hideSizeForBench0 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 0);
+    const hideSizeForBench1 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 1);
+    const hideSizeForBench2 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 2);
+    const hideSizeForBench3 = getBeforeBenchSize(rects, wrapperStartEmptySize, scrollRefCoordinates, scroll, firstRow, 3);
 
-  //   expect(renderRows.length).toBe(3);
-  // });
+    expect(firstRow).toBe(10); // 9: 450 ~ 500
+    expect(hideSizeForBench0).toBe(27); // 20
+    expect(hideSizeForBench1).toBe(77); // 50 + 20
+    expect(hideSizeForBench2).toBe(127); // 100 + 20
+    expect(hideSizeForBench3).toBe(177); // 150 + 20
+  });
+  // #endregion
 
-  test('fixed load', () => {
-    const rowCount = 1000;
-    const rowHeight = 50;
+  // #region utils styles
+  test('[utils.styles] getWrapperStyle test', () => {
     const body = document.body;
     const container = document.createElement('div');
     const wrapper = document.createElement('ul');
-    const item = document.createElement('li');
-    const rows: Record<string, any>[] = [];
 
-    container.scrollTop = 500;
     body.appendChild(container);
     container.appendChild(wrapper);
 
-    for (let i = 0; i < rowCount; i++) {
-      const newItem = item.cloneNode();
-
-      newItem.textContent = `${i + 1} 번째 항목입니다.`;
-
-      wrapper.appendChild(newItem);
-      rows.push({
-        row: i,
-        name: `${i + 1} 번째 항목입니다.`
-      });
-    }
-
-    const myVirtualScroll = new MyVirtualScroll(container, wrapper, {
-      rows,
-      rowHeight,
-    });
-
-    expect(1).toBe(rowCount / rowHeight);
+    const myVirtualScroll = new MyVirtualScroll(container, wrapper);
+    const styles = getWrapperStyle(wrapper, { transformFunctionName: 'translateY', size: 'height' }, { current: 100, size: 300 });
+   
+    expect(styles).toStrictEqual({ transform: 'translateY(100px)'}); // 150 + 20
   });
+  // #endregion
+
 });
